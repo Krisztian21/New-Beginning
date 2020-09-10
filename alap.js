8ad244c790b4ec60b84f225b935d96c5fb21d15e
Krisztian21 <krisztian.pasztor95@gmail.com>
2020-09-10 09:27:54 +0200

    Csütörtöki commit

---
+++ alap.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let nev="Polgár Lilla";
+
+/* Moduláris ablak függvények*/
+//window.alert(nev);
+//alert(nev);
+//let tanuloszam=("Hány tanuló van jelen?");
+//let szoveg=(`A tanulók száma: ${tanuloszam}`);
+
+let szoveg="Hello world!";
+//window.alert(szoveg);
+document.getElementById("kiirando").innerText=szoveg;
